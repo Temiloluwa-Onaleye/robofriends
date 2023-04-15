b0ed1d159c7dc75709833573cdc73cf8e05a9de2
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2023-04-15 14:33:42 +0100

    robofriends

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import "./App.css";
+import Scroll from "./Scroll";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
 
   onSearchChange = (e) => {
@@ -28,8 +38,10 @@ class App extends Component {
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <hr />
-        <CardList robots={filteredRobots} />
+
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     );
   }
